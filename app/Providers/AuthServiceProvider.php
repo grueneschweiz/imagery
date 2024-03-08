60d73a24737b26d5934f23ca70e6df9a90cedaa3
Sandro Tanner <sandro.tanner@gruene.ch>
2024-03-08 19:54:33 +0100

    upgrade laravel 9 -> 10

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,9 +26,10 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        $this->registerPolicies();
+        // https://laravel.com/docs/10.x/upgrade#register-policies
+        // $this->registerPolicies();
 
         //
     }
